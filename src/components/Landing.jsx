526fa71049b73ec1b64a357223fee3efde9462dc
moonjh9392 <moonjh9392@gmail.com>
2022-10-04 11:23:07 +0900

    todo list component

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,8 +1,20 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export default function Landing(params) {
-  const MainStyle = styled.div`
-    background-color: black;
+  const LandingStyle = styled.div`
+    background-color: whitesmoke;
+    height: 100vh;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   `;
-  return <MainStyle>dd</MainStyle>;
+  return (
+    <LandingStyle>
+      <div>
+        <Link to="/main">dd</Link>
+      </div>
+    </LandingStyle>
+  );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,42 @@
+import { Outlet } from 'react-router-dom';
 import styled from 'styled-components';
+import Header from './common/Header';
+import TodoList from './TodoList';
 
-export default function Main(params) {
-  return <></>;
-}
+const MainStyle = styled.div`
+  overflow-y: scroll;
+
+  .menu {
+    position: relative;
+    margin: 0 auto;
+    max-width: 718px;
+
+    @media screen and (max-width: 768px) {
+      padding: 128px 24px;
+      top: -128px;
+    }
+  }
+
+  .background {
+    background-color: whitesmoke;
+    padding-bottom: 80px;
+    padding-top: 40px;
+    height: 100vh;
+  }
+`;
+
+const Main = () => {
+  return (
+    <MainStyle>
+      <Header />
+      <div className="background">
+        <div className="menu">
+          {/* <Outlet /> */}
+          <TodoList />
+        </div>
+      </div>
+    </MainStyle>
+  );
+};
+
+export default Main;
