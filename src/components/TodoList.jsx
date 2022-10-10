f23087e169c698204a174dea56cf56e9ae40e7c4
moonjh9392 <moonjh9392@gmail.com>
2022-10-10 22:40:25 +0900

    write input

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,12 +3,28 @@ import styled from 'styled-components';
 
 export default function TodoList(params) {
   const [AddList, setAddList] = useState(null);
-  const TodoListStyle = styled.div``;
-  const AddInput = styled.input``;
+  const TodoListStyle = styled.div`
+    border: 1px solid rgba(231, 231, 234);
+    border-radius: 8px;
+    height: 48px;
+    display: flex;
+    justify-content: center;
+
+    input {
+      width: 100%;
+      height: 100%;
+      border: none;
+      padding: 0 18px;
+      border-radius: 8px;
+      font-size: 18px;
+    }
+  `;
+
+  const WriteButton = styled.button``;
   return (
     <TodoListStyle>
-      <AddInput />
-      <button />
+      <input placeholder="할 일을 입력하세요." />
+      <WriteButton />
     </TodoListStyle>
   );
 }
